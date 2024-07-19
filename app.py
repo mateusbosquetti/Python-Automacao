fa79d6285839456e96bfb65725d94d39f1bdc50d
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2024-07-19 12:10:55 -0300

    automacao

---
+++ app.py
@@ -0,0 +1,21 @@
+import pyautogui
+import keyboard
+import time
+import mouse
+
+contador = 0
+
+while True:
+    print(mouse.get_position())
+    time.sleep(1)
+    #Pegar info do mouse
+
+
+    # pyautogui.click(1808,452,duration=1.5)
+    # pyautogui.press
+    # # pyautogui.write()
+
+   
+    # pyautogui.click(1256,581,duration=1.5)
+    # contador = contador + 1
+    
